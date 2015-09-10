4900140c4d97dcea3dc74132bbf88934108835b4
System Administrator <justinkheisler@gmail.com>
2015-09-10 12:41:42 -0300

    adding redraw function for seismic

--- src/modules/seismic.js
+++ src/modules/seismic.js
@@ -25,12 +25,23 @@ g3.seismic = function(plot, data, options){
     return this;
   }
 
-	seismic.drawImage = function(canvas){
-		var context = canvas.node().getContext('2d'),
-		x = data.length,
-		y = data[0].length,
-		image = context.createImageData(x,y);
+	seismic.reDraw = function(data){
 
+  	// Update the Canvas Attributes
+  	seismic.canvas
+      .attr('width', data.length)
+      .attr('height', data[0].length)
+      .style('width', seismic.plot.width +  'px')
+      .style('height', seismic.plot.height + 'px');
+
+    // Wipe the old canvas, the new size can be different
+  	seismic.context.clearRect(0, 0, seismic.data.length, seismic.data[0].length);
+
+		var x = data.length,
+		y = data[0].length;
+		var image = seismic.context.createImageData(x,y);
+
+		// Paint the image
 		for(var i = 0, p = -1; i < y; ++ i){
 			for(var j = 0; j < x; ++j){
 				var c = d3.rgb(seismic.color(data[j][i]));
@@ -40,7 +51,28 @@ g3.seismic = function(plot, data, options){
 				image.data[++p] = 255;
 			}
 		}
-		context.putImageData(image, 0, 0);
+
+		// Dump image to canvas
+		seismic.context.putImageData(image, 0, 0);
+	  return this;
+  }
+
+	seismic.drawImage = function(canvas){
+		seismic.context = canvas.node().getContext('2d');
+		var x = seismic.data.length,
+		y = seismic.data[0].length;
+		seismic.image = seismic.context.createImageData(x,y);
+
+		for(var i = 0, p = -1; i < y; ++ i){
+			for(var j = 0; j < x; ++j){
+				var c = d3.rgb(seismic.color(seismic.data[j][i]));
+				seismic.image.data[++p] = c.r;
+				seismic.image.data[++p] = c.g;
+				seismic.image.data[++p] = c.b;
+				seismic.image.data[++p] = 255;
+			}
+		}
+		seismic.context.putImageData(seismic.image, 0, 0);
 		return this;
 	}
 
