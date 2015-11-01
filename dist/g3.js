a33f6e1a5306f9def7f34431b6dc15377c9e1534
System Administrator <justinkheisler@gmail.com>
2015-11-01 13:34:15 -0400

    adding rgb scale function to g3

--- dist/g3.js
+++ dist/g3.js
@@ -1,4 +1,4 @@
-/*! g3 - v0.0.1 - 2015-10-26 - justinkheisler */
+/*! g3 - v0.0.1 - 2015-11-01 - justinkheisler */
 'use strict';
 ;(function (window) {
 
@@ -1163,6 +1163,41 @@ plot.prototype.reDraw = function(xDomain, yDomain, x2Domain, y2Domain){
       .ease('linear');
   }
 };
+
+g3.scale = {};
+
+
+
+g3.scale.rgb = function(){
+  return new rgb();
+};
+
+var rgb = function(){
+	var	_domain = [[0, 1], [0, 1], [0, 1]],
+	_range = [[0, 255], [0, 255], [0, 255]];
+
+	function rgbScale(r, g, b){
+		var rScale = d3.scale.linear().domain(_domain[0]).range(_range[0]);
+		var gScale = d3.scale.linear().domain(_domain[1]).range(_range[1]);
+		var bScale = d3.scale.linear().domain(_domain[2]).range(_range[2]);
+		return d3.rgb(rScale(r), gScale(g), bScale(b));
+	}
+
+	rgbScale.domain = function(domain){
+		if(domain === undefined){ return _domain; }
+		_domain = domain;
+		return rgbScale;
+	};
+
+	rgbScale.range = function(range){
+		if(range === undefined){ return _range; }
+		_range = range;
+		return rgbScale;
+	}
+	return rgbScale;
+};
+
+
 
 // Attach canvas creation function to g3
 g3.scatter = function(plot, data){
